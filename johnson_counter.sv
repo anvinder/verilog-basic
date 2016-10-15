bac5219ec99430b461871cc5c6f663de1d5414c1
anvinder <anvinder_singh26@yahoo.co.in>
2016-10-14 22:45:29 -0700

    Create johnson_counter.sv

---
+++ johnson_counter.sv
@@ -0,0 +1,42 @@
+
+module sipo(clk, reset, q);
+input clk, reset;
+output logic [2:0]q;
+
+always @(posedge clk)
+begin
+if (reset==1)
+begin
+q<=3'b000;
+end
+else
+begin
+q<=3'b000;
+q <= {~q[0], q[2:1]};
+
+end
+end
+endmodule
+
+module dut();
+logic clk, reset;
+logic [2:0] q;
+sipo s(.clk(clk), .reset(reset), .q(q));
+
+always #20 clk=~clk;
+
+initial begin
+clk=0;
+reset=0;
+
+@(posedge clk)
+reset=1;
+@(posedge clk)
+
+reset=0;
+$monitor ("q=%b ", q);
+
+
+$finish;
+end
+endmodule
